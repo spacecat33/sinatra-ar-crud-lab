a9cfc699fc1804c897264247f6d8842e14913152
Janan Akkad <jananakkad@hotmail.co.uk>
2022-10-15 14:55:15 +0100

    commit tree f7d9b5e4bfa19f7e8f27c0ef9ffd3d19e30d119a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,49 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article = Article.create(params)
+    
+    redirect "articles/#{@article.id}"
+  end
+
+  
+  get '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by_id(params[:id])
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+
+    erb :index
   end
 end
